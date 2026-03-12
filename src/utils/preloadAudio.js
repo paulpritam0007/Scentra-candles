789dac9839c0c45c2d3fd245f83a1c7cef44ae7e
Pritam Paul <paul.pritam0007@gmail.com>
2026-03-12 21:33:40 +0530

    Update preloadAudio.js

--- src/utils/preloadAudio.js
+++ src/utils/preloadAudio.js
@@ -1,4 +1,4 @@
-export const paymentSound = new Audio("/audio/scentra-success.mp3");
+export const paymentSound = new Audio("brand-music.mp3");
 
 paymentSound.preload = "auto";
 paymentSound.load();
